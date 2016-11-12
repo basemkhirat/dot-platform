a4b0b2a3ab05bc376ae8f598f0cd7e5415f01aa7
basemkhirat <basemkhirat@gmail.com>
2016-11-12 14:31:48 +0400

    fixes

--- src/models/LangScope.php
+++ src/models/LangScope.php
@@ -10,10 +10,16 @@ class LangScope implements Scope
     public function apply(Builder $builder, Model $model)
     {
 
+        $lang = false;
+
         if(GUARD == "api"){
             $lang = Auth::guard("api")->user()->lang;
-        }else{
+        }elseif(defined("LANG")){
             $lang = LANG;
+        }
+
+        if($lang){
+            return $builder->where('posts.lang', $lang)->where("posts.status", 1);
         }
 
         return $builder->where('posts.lang', $lang);
